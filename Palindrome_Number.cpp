f17f7f8fc510ca8dd75aeff2d60fa436a1759b97
Aditya <aditdhayapulay@gmail.com>
2023-05-10 00:02:53 +0530

    Palindrome code has been added

---
+++ Palindrome_Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+#include<bits/stdc++.h>
+public:
+    bool isPalindrome(int x) {
+        if(x>=0)
+        {
+            long int temp,sum=0,rem;
+            temp = x;
+            while(x!=0)
+            {
+                rem = x % 10;
+                sum = sum * 10 + rem;
+                x=x/10;
+            }
+            if(sum==temp)
+            {
+                return true;
+            }
+            else
+            return false;
+        }
+        else
+        return false;
+    }
+};
